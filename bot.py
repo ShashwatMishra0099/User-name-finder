0e7bd193c955134785b8d16291a3cae82dd1a5c5
ShashwatMishra0099 <163753719+ShashwatMishra0099@users.noreply.github.com>
2024-03-31 15:25:03 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+TOKEN = "7073136015:AAEhuHGqeaEm33aNTtNHYmB8jBisAMQL1oA"
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Hello! I'm your Telegram bot. Use /members to get the list of members with usernames.")
+
+def members(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    members = context.bot.get_chat_members_count(chat_id)
+    member_list = context.bot.get_chat_members(chat_id)
+    response = "Total Members: {}\n".format(members)
+    response += "Members List:\n"
+    for member in member_list:
+        response += "{} - @{}\n".format(member.user.full_name, member.user.username)
+    update.message.reply_text(response)
+
+def main() -> None:
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("members", members))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
